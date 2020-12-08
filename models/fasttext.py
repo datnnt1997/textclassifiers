a0d0bfbce1de8948af0a07b2138b36b6abe479f0
datngo <dat821168@gmail.com>
2020-12-08 08:50:36 +0700

    _ update model Fasttext model

--- models/fasttext.py
+++ models/fasttext.py
@@ -6,14 +6,12 @@ class FastText(nn.Module):
         super(FastText, self).__init__()
         self.embed_layer = nn.Embedding(opts.vocab_size, opts.embed_dim, padding_idx=opts.pad_idx)
         self.fc1_layer = nn.Linear(opts.embed_dim, opts.num_labels)
-        # self.fc2_layer = nn.Linear(opts.hidden_dim, opts.num_labels)
         self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, input_ids, seq_len, label_ids=None):
         token_reps = self.embed_layer(input_ids)
         seq_reps = token_reps.mean(1)
         logits = self.fc1_layer(seq_reps)
-        # logits = self.fc2_layer(hiddens)
         probs = self.softmax(logits)
         if label_ids is None:
             return None, probs
